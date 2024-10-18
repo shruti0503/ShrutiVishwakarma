fef52f1ca5cb04952c0fc02fb70b997b6264a612
Shruti <94033084+shruti0503@users.noreply.github.com>
2024-10-18 16:48:19 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-// Hash the password before saving the user
+
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
